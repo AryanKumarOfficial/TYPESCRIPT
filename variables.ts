e86d2ef9ec081cda5fd6608c4e78c59dbce59bfe
Aryan Kumar <arynak9163@gmail.com>
2024-06-14 15:35:10 +0530

    variables

---
+++ variables.ts
@@ -0,0 +1,8 @@
+let greetings: string = "Hello Aryan";
+let myNum = 6;
+
+greetings.toUpperCase();
+
+console.log(greetings.toUpperCase());
+
+export {}; // This tells the compiler that this file is a module and not a script. This is done to avoid variable name conflicts.
