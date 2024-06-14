1e2a18951172d890eb35635d482fbc152c7fc9ca
Aryan Kumar <arynak9163@gmail.com>
2024-06-14 23:59:01 +0530

    type alias

---
+++ typeAlias.ts
@@ -0,0 +1,17 @@
+/**
+ * type alias in typescript
+ */
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function createUser(user: User): User {
+  return user;
+}
+
+createUser({ name: "", email: "", isActive: true });
+
+export {};
