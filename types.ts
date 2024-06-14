8154f87614f5827317535980844af60cbaabf2a7
Aryan Kumar <arynak9163@gmail.com>
2024-06-14 22:57:49 +0530

    numbers and booleans

---
+++ types.ts
@@ -0,0 +1,13 @@
+// numbers
+
+// let userId: number;
+// userId = 56 + 5 + 44; //not recomended
+
+let userId = 46446; // whecn immediately value assigned ts automatically checks the type saftey and no need to specify its type
+userId.toFixed();
+
+// booleans
+
+let loggedIn: boolean = true;
+console.log(loggedIn.valueOf());
+export {};
